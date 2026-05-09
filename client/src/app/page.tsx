e9ca2296837c0a40ad63443d336cefcc622e4224
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-05-09 17:04:50 +0600

    1st

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="text-center font-bold text-3xl pt-10">
+      <h1>Link Shortener</h1>
+    </div>
+    </>
+  );
+}
